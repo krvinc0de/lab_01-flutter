471f91d6d0e8f8dfc64f84f74515b7665d06b8dc
kevs codes <polopolopolopoline@gmail.com>
2023-12-06 00:40:07 -0600

    practica2

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,23 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    title: 'Prueba flutter noob',
-    home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Prueba flutter noob'),
-          backgroundColor: Colors.blueGrey[900],
-        ),
-        body: const Center(
-          child: Image(
-              image: NetworkImage(
-                  'https://lh5.googleusercontent.com/IFtWijivj1-dWPLsA_gvUAji6fayJ3Y_TP88dZ4TCx480vk726r8C9VlLh3hrrafKU1DGiBwmF-yF0JgaDuuwpM=w16383')),
-        )),
-  ));
+  runApp(const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Prueba flutter noob pt2',
+      home: Stack(
+        children: [
+          Image(
+            image: NetworkImage(
+                'https://t4.ftcdn.net/jpg/03/78/40/11/360_F_378401105_9LAka9cRxk5Ey2wwanxrLTFCN1U51DL0.jpg'),
+            fit: BoxFit.cover,
+            height: double.infinity,
+            width: double.infinity,
+            alignment: Alignment.center,
+          ),
+          Center(
+              child: Image(
+                  image: NetworkImage(
+                      'https://lh5.googleusercontent.com/IFtWijivj1-dWPLsA_gvUAji6fayJ3Y_TP88dZ4TCx480vk726r8C9VlLh3hrrafKU1DGiBwmF-yF0JgaDuuwpM=w16383'))),
+        ],
+      )));
 }
