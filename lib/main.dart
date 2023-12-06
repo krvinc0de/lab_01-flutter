26cf4f9326b8065bf2e8496f2c61f3b39179e9b1
kevs codes <polopolopolopoline@gmail.com>
2023-12-06 01:05:17 -0600

    update imagelink

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,9 @@ void main() {
         ),
         body: const Center(
           child: Image(
-              image: NetworkImage(
-                  'https://lh5.googleusercontent.com/IFtWijivj1-dWPLsA_gvUAji6fayJ3Y_TP88dZ4TCx480vk726r8C9VlLh3hrrafKU1DGiBwmF-yF0JgaDuuwpM=w16383')),
+            image: NetworkImage(
+                'https://www.utsalamanca.edu.mx/assets/img/pagina-principal/logouts.png'),
+          ),
         )),
   ));
 }
