4cf6ca936cf11c05a622b00ccf30d0b0838fb390
kevscodes <polopolopolopoline@gmail.com>
2023-12-06 00:28:48 -0600

    practica1 - imagen uts

--- lib/main.dart
+++ lib/main.dart
@@ -1,68 +1,18 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'imagen centrada de flutter',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'imagen de flutter'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Stack(
-      children: <Widget>[
-        Image.network(
-          'https://flutter.dev/images/flutter-logo-sharing.png',
-          height: MediaQuery.of(context).size.height,
-          width: MediaQuery.of(context).size.width,
-          fit: BoxFit.cover,
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: 'Prueba flutter noob',
+    home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Prueba flutter noob'),
+          backgroundColor: Colors.blueGrey[900],
         ),
-        Scaffold(
-          backgroundColor: Colors.transparent,
-          appBar: AppBar(
-            title: const Text('imagen de flutter'),
-            backgroundColor: Colors.deepPurple,
-          ),
-          body: const Text(
-            'imagen de flutter',
-            style: TextStyle(
-              fontSize: 30,
-              fontWeight: FontWeight.bold,
-              color: Colors.black,
-            ),
-          ),
-        )
-      ],
-    );
-  }
+        body: const Center(
+          child: Image(
+              image: NetworkImage(
+                  'https://lh5.googleusercontent.com/IFtWijivj1-dWPLsA_gvUAji6fayJ3Y_TP88dZ4TCx480vk726r8C9VlLh3hrrafKU1DGiBwmF-yF0JgaDuuwpM=w16383')),
+        )),
+  ));
 }
-
-
-// const Center(
-//               child: Image(
-//                   image: NetworkImage(
-//                       'https://logowik.com/content/uploads/images/flutter5786.jpg'))),
