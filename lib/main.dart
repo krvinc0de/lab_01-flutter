1cf4cd18a59118bfefdc490882ea1fdbb37ad190
kevs codes <polopolopolopoline@gmail.com>
2023-12-06 01:07:05 -0600

    update image

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ void main() {
           Center(
               child: Image(
                   image: NetworkImage(
-                      'https://lh5.googleusercontent.com/IFtWijivj1-dWPLsA_gvUAji6fayJ3Y_TP88dZ4TCx480vk726r8C9VlLh3hrrafKU1DGiBwmF-yF0JgaDuuwpM=w16383'))),
+                      'https://www.utsalamanca.edu.mx/assets/img/pagina-principal/logouts.png'))),
         ],
       )));
 }
